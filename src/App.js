ecafc9c56ca792548d512ac77aaf7b18f2bd3120
Kal-MH <rkfalgus713@gmail.com>
2020-04-23 20:40:52 +0900

    chapter.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from "react";
-import Potato from "./Potato";
+import PropTypes from "prop-types";
+
+function Food({ name, rating }) {
+  return (
+    <div>
+      <h3>
+        I like {name}, {rating}/5.0
+      </h3>
+    </div>
+  );
+}
+
+const foods = [
+  {
+    id: 1,
+    name: "kimchi",
+    rating: 4.0,
+  },
+  {
+    id: 2,
+    name: "goggi",
+    rating: 4.8,
+  },
+  {
+    id: 3,
+    name: "된장찌개",
+    rating: 5.0,
+  },
+];
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
-      <Potato />
+      {foods.map((dish) => (
+        <Food key={dish.id} name={dish.name} rating={dish.rating} />
+      ))}
     </div>
   );
 }
